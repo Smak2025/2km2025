f8facd74bb7ab7a57300702a6c3d1e4af72ff2ee
Smak2025 <smak-80+2025@yandex.ru>
2025-11-05 19:08:59 +0300

    Начало работы с БД

--- common/db_helper.php
+++ common/db_helper.php
@@ -18,9 +18,7 @@ class DbHelper
         $conn_str  = "pgsql:host=" . self::$host;
         $conn_str .= ";port=" . self::$port;
         $conn_str .= ";dbname=" . self::$dbname;
-        $conn_str .= ";username=" . self::$username;
-        $conn_str .= ";password=" . self::$password;
-        $this->conn = new PDO($conn_str);
+        $this->conn = new PDO($conn_str, self::$username, self::$password);
     }
 
     public static function getInstance(): DbHelper{
@@ -28,6 +26,52 @@ class DbHelper
             self::$dbHelper = new DbHelper();
         }
         return self::$dbHelper;
+    }
+
+    public function createStudentTable(){
+        $query  = 'CREATE TABLE IF NOT EXISTS students ('.
+                  'id SERIAL PRIMARY KEY, '.
+                  'firstName VARCHAR(30) NOT NULL, '.
+                  'lastName VARCHAR(30) NOT NULL, '.
+                  'birthDate DATE NOT NULL, '.
+                  'groupNum VARCHAR(10) NOT NULL)';
+        try{
+            $this->conn->beginTransaction();
+            $this->conn->exec($query);
+            $this->conn->commit();
+        } catch (\PDOException $e){
+            $this->conn->rollBack();
+        }
+    }
+
+    public function addStudent($firstName, $lastName, $birthDate, $groupNum): void
+    {
+        $query  = 'INSERT INTO students (firstName, lastName, birthDate, groupNum) VALUES ('.
+            ':firstName, :lastName, :birthDate, :groupNum)';
+        try {
+            $this->conn->beginTransaction();
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':firstName', $firstName);
+            $stmt->bindParam(':lastName', $lastName);
+            $stmt->bindParam(':birthDate', $birthDate);
+            $stmt->bindParam(':groupNum', $groupNum);
+            $stmt->execute();
+            $this->conn->commit();
+        } catch (\PDOException $e) {
+            $this->conn->rollBack();
+        }
+    }
+
+    public function addStudentBad($firstName, $lastName, $birthDate, $groupNum){
+        $query  = 'INSERT INTO students (firstName, lastName, birthDate, groupNum) VALUES ('.
+            "'$firstName', '$lastName', '$birthDate', '$groupNum')";
+        try {
+            $this->conn->beginTransaction();
+            $this->conn->exec($query);
+            $this->conn->commit();
+        } catch (\PDOException $e) {
+            $this->conn->rollBack();
+        }
     }
 
 }
---
+++ db_test.php
@@ -0,0 +1,32 @@
+<?php
+
+use common\DbHelper;
+
+include_once "common/content.php";
+include_once "common/page.php";
+include_once "common/db_helper.php";
+class DbTest extends \common\AContent
+{
+    private ?DbHelper $helper;
+    private ?string $conn_error = null;
+    public function __construct()
+    {
+        parent::__construct();
+        try {
+            $this->helper = DbHelper::getInstance();
+            $this->helper->createStudentTable();
+            $this->helper->addStudent('Иван', 'Иванов', "2001-05-30", '05-410');//\');DELETE FROM students;--');
+        } catch (\PDOException $e) {
+            $this->conn_error = $e->getMessage();
+        }
+    }
+
+    public function create_content()
+    {
+        if ($this->conn_error != null) {
+            print "<div class='alert alert-danger'>$this->conn_error</div>";
+        }
+    }
+}
+
+new \common\APage(new DbTest());
